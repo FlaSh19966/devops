4a830c2849726e89ec9674021b0ab51bf8701c54
Vimalesh Dhamale <vimalesh.d@codearray.tech>
2022-12-13 15:08:57 +0530

    Feat:nginx configured

--- devops_app/minimal_app.py
+++ devops_app/minimal_app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 api = Api(app)
 
 
+class Default(Resource):
+    def get(self):
+        print('Default endpoint')
+        return make_response(jsonify({'MSG': 'This is default endpoint'}), 200)
+
+
 class FlaskHealthCheck(Resource):
     def get(self):
         print('flask-health-check hit')
@@ -19,9 +25,10 @@ class NginxHealthCheck(Resource):
         return make_response(jsonify({'MSG': 'This is nginx health check'}), 200)
 
 
+api.add_resource(Default, '/')
 api.add_resource(FlaskHealthCheck, '/health-check')
 api.add_resource(NginxHealthCheck, '/v1/health-check')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
