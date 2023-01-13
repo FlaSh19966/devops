44319766cdb93e382a92c027cd4fefed008c999f
Vimalesh Dhamale <vimalesh.d@codearray.tech>
2023-01-13 12:56:46 +0530

    Feat: basic changes

--- minimal_app.py
+++ minimal_app.py
@@ -3,12 +3,12 @@ import datetime
 from flask import Flask, make_response, jsonify
 from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
-
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config["MONGO_URI"] = "mongodb://mongo-service:27017/dev"
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 db = mongo.db
@@ -18,8 +18,9 @@ class Default(Resource):
     def get(self):
         print('Default endpoint')
         endpoint_name = 'default'
+        name = request
         try:
-            db.test.insert_one({'endpoint_name': endpoint_name, 'datetime': datetime.datetime.now()})
+            db.test.insert_one({'name': name, 'datetime': datetime.datetime.now()})
             data = list(db.test.find({}, {'_id': 0}))
             return make_response(jsonify({'MSG': 'This is default endpoint',
                                           'data': data}), 200)
