d09a8abbfe5fb116818e40d118f21a72ab4dd5dc
Vimalesh Dhamale <vimalesh.d@codearray.tech>
2023-01-13 15:25:38 +0530

    test: jenkins test 1

--- devops_app/minimal_app.py
+++ devops_app/minimal_app.py
@@ -22,7 +22,7 @@ class FlaskHealthCheck(Resource):
 class NginxHealthCheck(Resource):
     def get(self):
         print('nginx-health-check hit')
-        return make_response(jsonify({'MSG': 'This is nginx health check 3.0'}), 200)
+        return make_response(jsonify({'MSG': 'This is nginx health check 4.0'}), 200)
 
 
 api.add_resource(Default, '/')
